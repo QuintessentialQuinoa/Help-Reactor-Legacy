387610d412ef180f7f4267a6e64f579f651d760a
pboserman <pabloboserman@gmail.com>
2017-07-06 09:15:01 -0700

    (feat) adds link to user profile page

--- client/components/onlineUserEntry.jsx
+++ client/components/onlineUserEntry.jsx
@@ -5,7 +5,7 @@ const OnlineUserEntry = ({user}) => {
   return (
     <div className="modal-entry-container">
     <img className="modal-entry-img" src={user.avatarUrl} />
-    <span className="modal-entry-name">{user.firstName} {user.lastName}</span> - <span className="modal-entry-username">{user.username}</span>
+    <a href={`https://www.github.com/${user.username}`} target="_blank"><span className="modal-entry-name">{user.firstName} {user.lastName}</span></a> - <span className="modal-entry-username">{user.username}</span>
     </div>
   );
 };
