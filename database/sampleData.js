e2772b0b8f9655fcf111959519d3891b2f2202de
pboserman <pabloboserman@gmail.com>
2017-07-08 14:59:03 -0700

    (feat) adds me as admin and adds role in navbar

--- database/sampleData.js
+++ database/sampleData.js
@@ -31,7 +31,7 @@ module.exports.users = [
     "firstName": "Pablo",
     "lastName": "Boserman",
     "username": "pboserman",
-    "role": "mentor",
+    "role": "admin",
     "cohort": "HRSF-77"
   },
   {
